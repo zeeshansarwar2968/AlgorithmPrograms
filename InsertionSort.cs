1e3cf8be98e4786f44d1d504080a9b469643b0c8
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-31 17:12:55 +0530

    [Zeeshan] : Added logic for insertion sort execution

---
+++ InsertionSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    internal class InsertionSort
+    {
+        //Algo: The array is virtually split into a sorted and an unsorted part.
+        //Values from the unsorted part are picked and placed at the correct position in the sorted part.
+        public void InsertionSortAlgo()
+        {
+            //Taking in value to define size of array 
+            Console.Write("Please enter the size of array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            //Initialisation of an array
+            string[] array = new string[size];
+
+            //Taking in array elements from the user
+            Console.WriteLine("Please enter the string elements of array:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter Value : ");
+                array[i] = Console.ReadLine();
+            }
+
+            //Looping through the array and comparing the values/elements
+            for (int i = 1; i < array.Length; i++)
+            {
+                //element is initialised for each loop
+                string key = array[i];
+                //variable j is initialised to point to the last elemnt in the array 
+                int j = i - 1;
+                //running the loop for each condition where the previous value is greater, the lements are flipped in the aray to meet the condition
+                while ((j >= 0) && (array[j].CompareTo(key) > 0))
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                //if the above loop is executed, then the next element is assigned the preosent element value
+                array[j + 1] = key;
+            }
+            Console.WriteLine("Displaying the sorted Array : ");
+            //Display the elements in the array
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace AlgorithmProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Algorithm Problems");
+            Console.WriteLine("\t\t\t\t\t Algorithm Problems");
+            Console.WriteLine("\t\t---------------------------------------------------------------------");
+            Console.WriteLine("1 :: Insertion Sort Implementation");
+            Console.Write("Please Enter from the options above to start execution : ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Exit");
+                    break;
+                case 1:
+                    Console.WriteLine();
+                    InsertionSort insertionSort = new InsertionSort();
+                    insertionSort.InsertionSortAlgo();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
